000a43fd1d8a8bc47f79038d863d07ad4f2b274f
Richard Pardo-Figueroa <95098795+rparfig@users.noreply.github.com>
2022-08-29 10:06:17 -0700

    Delete known Vault path

--- terraform-vault-cred-create/variables.tf
+++ terraform-vault-cred-create/variables.tf
@@ -1,6 +1,5 @@
 variable "vault_addr" {
   description = "address of the Vault server"
-  #  default = "http://kubernetes-vault-elb-232188935.us-east-1.elb.amazonaws.com:8200"
   default = "http://127.0.0.1:8200"
 }
 
